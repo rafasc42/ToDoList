c28f276bcfa64e5d9508294b961c6fe7d7babb32
Rafael Borges <rafaelborges.sc@hotmail.com>
2023-01-27 01:56:30 -0400

    added local storage feature

--- code.js
+++ code.js
@@ -1,5 +1,28 @@
 let addButton = document.querySelector("#submit");
+let delAll = document.querySelector("#deleteAll");
 let listEl = document.querySelector("#task-list");
+
+window.onload = function(){
+    for (let i = 0; i < localStorage.length; i++) {
+        let item = document.createElement("li");
+        let pEl = document.createElement("p");
+        let delButton = document.createElement("button");
+        delButton.textContent = "x";
+        let key = localStorage.key(i);
+        let inputText = localStorage.getItem(key);
+        pEl.textContent = inputText;
+        item.setAttribute("data-key", key);
+        item.appendChild(pEl);
+        item.appendChild(delButton);
+        listEl.appendChild(item);
+        delButton.addEventListener('click', (event)=>{
+            delTask(event);
+        });
+        pEl.addEventListener('click', (event)=>{
+            editTask(event);
+        });
+    }
+};
 
 function addTask()
 {
@@ -9,6 +32,10 @@ function addTask()
         let item = document.createElement("li");
         let pEl = document.createElement("p");
         let delButton = document.createElement("button");
+        delButton.textContent = "x";
+        let key = Date.now().toString();
+        localStorage.setItem(key, inputText.value);
+        item.setAttribute("data-key", key);
         pEl.innerHTML = inputText.value;
         item.appendChild(pEl);
         item.appendChild(delButton);
@@ -28,12 +55,30 @@ function addTask()
 function editTask(event){
     let pEl = event.target;
     pEl.contentEditable = true;
-    pEl.focus();
+    pEl.addEventListener("blur", ()=>{
+    let key = pEl.parentElement.getAttribute("data-key");
+    let newText = pEl.textContent;
+
+    localStorage.setItem(key, newText);
+    pEl.contentEditable = false;
+
+    });
 }
 
 function delTask(event){
-    let delButton = event.target;
-    delButton.parentNode.remove();
+    let parent = event.target.parentElement;
+    let key = parent.getAttribute("data-key");
+
+    localStorage.removeItem(key);
+    parent.remove();
+    // let delButton = event.target;
+    // delButton.parentNode.remove();
+}
+
+function deleteAll(){
+    localStorage.clear();
+    location.reload();
 }
 
 addButton.addEventListener('click', addTask);
+delAll.addEventListener('click', deleteAll)
